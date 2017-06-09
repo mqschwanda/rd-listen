121778aac79e81c85544c96b171988dc3354ccad
mqschwanda <mqschwanda@gmail.com>
2017-06-09 12:23:27 -0400

    remove scss

--- imports/modules/stylesheet.js
+++ imports/modules/stylesheet.js
@@ -3,6 +3,20 @@ const borderColor = '#393939';
 const backgroundColor = '#000';
 
 export default `
+  @font-face {
+    font-family:'Futura';
+    src: url('/fonts/FuturaStd-CondensedBold_gdi.eot');
+    src: url('/fonts/FuturaStd-CondensedBold_gdi.eot?#iefix') format('embedded-opentype'),
+      url('/fonts/FuturaStd-CondensedBold_gdi.woff') format('woff'),
+      url('/fonts/FuturaStd-CondensedBold_gdi.ttf') format('truetype'),
+      url('/fonts/FuturaStd-CondensedBold_gdi.otf') format('opentype'),
+      url('/fonts/FuturaStd-CondensedBold_gdi.svg#FuturaStd-CondensedBold') format('svg');
+    font-weight: 700;
+    font-style: normal;
+    font-stretch: condensed;
+    unicode-range: U+0020-00FE;
+  }
+
   .banner {
     width: 100%;
     height: auto;
--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -4,7 +4,7 @@
   Render the `<App />` layout inside of the react root created in the client's
   `main.html` file.
 **/
-
+import 'normalize.css';
 import { Meteor } from 'meteor/meteor';
 import React from 'react';
 import { render } from 'react-dom';
