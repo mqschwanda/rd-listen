fb3712b3e26a2951ad2dd420ad2451a3fa90c6cc
mqschwanda <mqschwanda@gmail.com>
2017-07-07 13:41:32 -0400

    call file from node babel

--- scripts/postbuild.js
+++ scripts/postbuild.js
@@ -1,5 +1,6 @@
 import { exec } from 'shelljs';
 
 console.log('Building App for heroku...');
-exec('NODE_ENV=production && babel-node ./scripts/setup.js');
+// exec('NODE_ENV=production && babel-node ./scripts/setup.js');
+exec('NODE_ENV=production && ./node_modules/babel-cli/bin/babel-node.js ./scripts/setup.js');
 console.log('Build Complete!');
