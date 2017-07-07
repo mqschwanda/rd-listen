72d0f0ef5ca7195d634ca1b73758757f4c5bec48
mqschwanda <mqschwanda@gmail.com>
2017-07-07 13:36:42 -0400

    update server startup to es6

--- imports/startup/server/index.js
+++ imports/startup/server/index.js
@@ -1,10 +1,8 @@
-/* eslint-disable no-var, prefer-arrow-callback, prefer-template */
+import express from 'express';
+import path from 'path';
 
-var express = require('express');
-var path = require('path');
-
-var app = express();
-var publicDir = path.resolve(__dirname, '../../../', 'public');
+const app = express();
+const publicDir = path.resolve(__dirname, '../../../public');
 
 app.use('/', express.static(publicDir));
 app.listen(process.env.PORT || 8080);
