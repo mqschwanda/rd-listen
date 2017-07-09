6d462e31a59b425440b0b458827d52c4ac388444
mqschwanda <mqschwanda@gmail.com>
2017-07-09 03:03:25 -0400

    tweak webpack build script

--- scripts/helpers/bash-scripts.js
+++ scripts/helpers/bash-scripts.js
@@ -1,7 +1,7 @@
 export default {
   dev: 'NODE_ENV=development && node -r babel-register ./node_modules/webpack-dev-server/bin/webpack-dev-server -d --progress --colors --watch --config ./config/webpack.babel.js',
   lint: 'eslint ./**.js',
-  buildWebpack: './node_modules/.bin/webpack -p --progress --colors --config ./config/webpack.babel.js',
+  buildWebpack: ' node -r babel-register ./node_modules/.bin/webpack -p --progress --colors --config ./config/webpack.babel.js',
   startServer: 'NODE_ENV=production && node -r babel-register ./imports/startup/server/index.js',
   test: 'echo Error: no test specified && exit 1',
 };
