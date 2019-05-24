a4a77fd7b56ce40867b87842ccd014bb4c5ec331
mqschwanda <mqschwanda@gmail.com>
2019-05-24 19:28:43 -0400

    add music choice

--- imports/modules/content/index.js
+++ imports/modules/content/index.js
@@ -5,6 +5,7 @@ export default [{
   banner: aws('radio-disney.png'),
   list: [
     'iHEART RADIO',
+    'MUSIC CHOICE',
     'RADIODISNEY.COM',
     'SIRIUSXM SATELLITE RADIO',
     'TUNEIN RADIO',
